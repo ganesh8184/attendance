c9e0fff1351b1e61807ae5d9dff7ad5b07b39b67
Ganesh <ganesh.hargude@avantecodeworx.com>
2025-06-04 11:01:51 +0530

    latest

--- markin.php
+++ markin.php
@@ -1,82 +1,113 @@
-<?php 
-$date=date("Y-m-d");
-// echo $date;
-if($date == "2024-01-26" || $date == "2024-04-09" ||  $date == "2024-04-11" || $date == "2024-05-01" || $date == "2024-08-15" || $date == "2024-09-17" || $date == "2024-10-02" || $date == "2024-10-31" || $date == "2024-11-01"|| $date == "2024-12-25")
-{
-    // echo "hi";die;
-}
-else
-{
-if(isset($_GET['issleep']))
-
-{
-
-    
-
-}
-
-else
-
-{
-
-  sleep(rand(60,300));
-
-}
-//include("/home/timesheetavante/public_html/invoice/connection.php");
-
-session_start();
-//get token 
-    $username="innoULBjwtclientid";
-    $password="ITInnowave20!&MU";
-
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/oauth/token");
-	curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "username=innowave&password=Innowave@123$&grant_type=password");
-    curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);  
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$output = curl_exec($ch);
-	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	curl_close($ch);
-	$json = json_decode($output, true);			
-	$access_token = $json['access_token'];
-// 	echo $access_token;die;
-	
-// Mark In 
-    $data = array("currentLat"=> "18.5200406","currentLon"=> "73.7785363","hrmsEmpId"=> 119245,"locationName"=> "Bavdhan Office,","ulbId"=> 707,"deviceFrom"=>"I","deviceImeiNoIn"=>"00008110-000135E40A07A01E");
-
-    $postdata = json_encode($data);
-    $authorization = "Authorization: Bearer ".$access_token;
-    $ch1 = curl_init();
-	curl_setopt($ch1, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/hrms/rest/geo/save/emp/attendance/intime_flutter");
-	curl_setopt($ch1, CURLOPT_POST, 1);
-    curl_setopt($ch1, CURLOPT_POSTFIELDS,$postdata);
-    curl_setopt($ch1, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
-	curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
-	$output1 = curl_exec($ch1);
-	echo $output1;
-	$http_code1 = curl_getinfo($ch1, CURLINFO_HTTP_CODE);
-	curl_close($ch1);
-	$json1 = json_decode($output1, true);			
-	// $attId=$json1['attId'];
-	// $_SESSION["attId"] = $attId;
-// 	$sql_insert = "INSERT INTO `attendace_mark`(`a_empid`, `a_attid`, `a_status`, `a_added_date`) VALUES (1004,".$attId.",0,NOW())";
-      
-//     $conn->exec($sql_insert);
-	
-// 	$file="/home/timesheetavante/public_html/attId.txt";
-//     $fp = fopen($file,"w");
-//     fwrite($fp,$attId);
-//     fclose($fp);
-    
-    
-    
-    
-	// echo $attId;
-}
-?>
+<?php
+
+$date = date("Y-m-d");
+
+$holidays = [
+    "2024-01-26", "2024-04-09", "2024-04-11", "2024-05-01",
+    "2024-08-15", "2024-09-17", "2024-10-02", "2024-10-31",
+    "2024-11-01", "2024-12-25"
+];
+
+if (in_array($date, $holidays)) {
+    // It's a holiday - exit or do nothing
+    exit;
+}
+
+// if (!isset($_GET['issleep'])) {
+//     sleep(rand(60, 300));
+// }
+
+session_start();
+
+$username = "innoULBjwtclientid";
+$password = "ITInnowave20!&MU";
+
+function getAccessToken($username, $password) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/oauth/token");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "username=innowave&password=Innowave@123$&grant_type=password");
+    curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    $output = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $err = curl_error($ch);
+        curl_close($ch);
+        throw new Exception("Error getting access token: $err");
+    }
+
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($http_code !== 200) {
+        throw new Exception("Failed to get token, HTTP code: $http_code, Response: $output");
+    }
+
+    $json = json_decode($output, true);
+    if (!isset($json['access_token'])) {
+        throw new Exception("Access token not found in response");
+    }
+
+    return $json['access_token'];
+}
+
+function postCurlRequest($url, $data, $access_token) {
+    $postdata = json_encode($data);
+    $authorization = "Authorization: Bearer " . $access_token;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    $output = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $err = curl_error($ch);
+        curl_close($ch);
+        throw new Exception("cURL POST error: $err");
+    }
+
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($http_code !== 200) {
+        throw new Exception("HTTP error during POST: Code $http_code, Response: $output");
+    }
+
+    return json_decode($output, true);
+}
+
+try {
+    $access_token = getAccessToken($username, $password);
+
+    $data = [
+        "currentLat" => "18.5200406",
+        "currentLon" => "73.7785363",
+        "hrmsEmpId" => 119245,
+        "locationName" => "Bavdhan Office,",
+        "ulbId" => 707,
+        "deviceFrom" => "I",
+        "deviceImeiNoIn" => "00008110-000135E40A07A01E"
+    ];
+
+    $url = "http://210.89.42.212:9999/HRMSAPI/hrms/rest/geo/save/emp/attendance/intime_flutter";
+
+    $response = postCurlRequest($url, $data, $access_token);
+
+    echo json_encode($response);
+
+} catch (Exception $e) {
+    echo json_encode([
+        "error" => true,
+        "message" => $e->getMessage()
+    ]);
+}
--- markout.php
+++ markout.php
@@ -1,131 +1,127 @@
-<?php 
-$date=date("Y-m-d");
-// echo $date;die;
-if($date == "2024-01-26" || $date == "2024-04-09" || $date == "2024-04-11" ||  $date == "2024-05-01" || $date == "2024-08-15" || $date == "2024-09-17" || $date == "2024-10-02" || $date == "2024-10-31" || $date == "2024-11-01"|| $date == "2024-12-25")
-{
-    // echo "hi";die;
-}
-else
-{
-if(isset($_GET['issleep']))
-{
-}
-else
-{
-  sleep(rand(60,300));
-}
-//include("/home/timesheetavante/public_html/invoice/connection.php");
- // sleep(rand(60,300));
-session_start();
-//get token 
-    $username="innoULBjwtclientid";
-    $password="ITInnowave20!&MU";
-
-	$ch = curl_init();
-	//curl_setopt($ch, CURLOPT_URL, "https://hr.avantecodeworx.com/HRMSAPI/oauth/token");
-	curl_setopt($ch, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/oauth/token");
-	curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "username=innowave&password=Innowave@123$&grant_type=password");
-    curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);  
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$output = curl_exec($ch);
-	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	curl_close($ch);
-	$json = json_decode($output, true);			
-	$access_token = $json['access_token'];
-	// echo $access_token;die;
-	
-	
-    $data = array("attid"=> "0","hrmsEmpId"=> 119245,"ulbId"=> 707);
-
-    $postdata = json_encode($data);
-    $authorization = "Authorization: Bearer ".$access_token;
-    $ch1 = curl_init();
-	//curl_setopt($ch1, CURLOPT_URL, "https://hr.avantecodeworx.com/HRMSAPI/hrms/rest/geo/emp/attendance/ismark/timeoutV1/det");
-	curl_setopt($ch1, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/hrms/rest/geo/emp/attendance/ismark/timeoutV1/det");
-	curl_setopt($ch1, CURLOPT_POST, 1);
-    curl_setopt($ch1, CURLOPT_POSTFIELDS,$postdata);
-    curl_setopt($ch1, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
-	curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
-	$output1 = curl_exec($ch1);
-	$http_code1 = curl_getinfo($ch1, CURLINFO_HTTP_CODE);
-	curl_close($ch1);
-	$json1 = json_decode($output1, true);
-	$attId= $json1['attid'];
-	
-	
-	
-	
-	
-   $ch = curl_init();
-	//curl_setopt($ch, CURLOPT_URL, "https://hr.avantecodeworx.com/HRMSAPI/oauth/token");
-	curl_setopt($ch, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/oauth/token");
-	curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-            "username=innowave&password=Innowave@123$&grant_type=password");
-    curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);  
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$output = curl_exec($ch);
-	$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	curl_close($ch);
-	$json = json_decode($output, true);			
-	$access_token = $json['access_token'];
-//	echo $access_token;die;
-	
-// 	$file="/home/timesheetavante/public_html/attId.txt";
-// 	$myfile = fopen($file, "r") or die("Unable to open file!");
-//     $attId=fread($myfile,filesize($file));
-//     fclose($myfile);
-    
-    
-// // Mark Out 
-//     if($attId==""||$attId==NULL||$attId==null||$attId="null")
-//     {
-//         $inv_qry = "SELECT  `a_empid`, `a_attid`, `a_status`, `a_added_date` FROM `attendace_mark` order by a_added_date desc limit 1";
-      
-//         try {
-//             $statement3 = $conn->query($inv_qry);
-//             $attendance    = $statement3->fetch(PDO::FETCH_ASSOC);
-            
-//             } catch (PDOException $ex) {
-//             print_r($ex->getMessage());
-//             die;
-//             } finally{
-//             $statement = null;
-//             //$conn = null;
-//             }
-//             $attId=$attendance['a_attid'];
-//         }
-
-
-    $data = array("attid"=>$attId,"outtimeLat"=>"18.5199552","outtimeLon"=>"73.778571","hrmsEmpId"=> 119245,"outtime_locationName"=> "NDA Pashan Road, Bavdhan, Pune, Maharashtra, India","ulbId"=> 707,"deviceFrom"=>"I","deviceImeiNoIn"=>"00008110-000135E40A07A01E","locationName"=>"null");
-
-    $postdata = json_encode($data);
-    // echo $postdata;die;
-    $authorization = "Authorization: Bearer ".$access_token;
-    $ch1 = curl_init();
-	//curl_setopt($ch1, CURLOPT_URL, "https://hr.avantecodeworx.com/HRMSAPI/hrms/rest/geo/save/emp/attendance/outtime");
-	curl_setopt($ch1, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/hrms/rest/geo/save/emp/attendance/outtime");
-	curl_setopt($ch1, CURLOPT_POST, 1);
-    curl_setopt($ch1, CURLOPT_POSTFIELDS,$postdata);
-    curl_setopt($ch1, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
-	curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
-	$output1 = curl_exec($ch1);
-	$http_code1 = curl_getinfo($ch1, CURLINFO_HTTP_CODE);
-	curl_close($ch1);
-	$json1 = json_decode($output1, true);			
-// 	$attId=$json1['attId'];
-// 	$_SESSION["attId"] = $attId;
-echo $output1;
-
-}
+<?php 
+
+$date = date("Y-m-d");
+
+$holidays = [
+    "2024-01-26", "2024-04-09", "2024-04-11", "2024-05-01",
+    "2024-08-15", "2024-09-17", "2024-10-02", "2024-10-31",
+    "2024-11-01", "2024-12-25"
+];
+
+if (in_array($date, $holidays)) {
+    // Holiday - do nothing or exit early
+    exit;
+}
+
+// if (!isset($_GET['issleep'])) {
+//     sleep(rand(60, 300));
+// }
+
+session_start();
+
+$username = "innoULBjwtclientid";
+$password = "ITInnowave20!&MU";
+
+function getAccessToken($username, $password) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://210.89.42.212:9999/HRMSAPI/oauth/token");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "username=innowave&password=Innowave@123$&grant_type=password");
+    curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    $output = curl_exec($ch);
+    if (curl_errno($ch)) {
+        curl_close($ch);
+        throw new Exception("cURL error while fetching token: " . curl_error($ch));
+    }
+
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($http_code !== 200) {
+        throw new Exception("HTTP error while fetching token: Code $http_code, Response: $output");
+    }
+
+    $json = json_decode($output, true);
+    if (!isset($json['access_token'])) {
+        throw new Exception("Access token not found in response");
+    }
+
+    return $json['access_token'];
+}
+
+function postCurl($url, $data, $access_token) {
+    $postdata = json_encode($data);
+    $authorization = "Authorization: Bearer " . $access_token;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+    $output = curl_exec($ch);
+    if (curl_errno($ch)) {
+        curl_close($ch);
+        throw new Exception("cURL error while POST: " . curl_error($ch));
+    }
+
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($http_code !== 200) {
+        throw new Exception("HTTP error while POST: Code $http_code, Response: $output");
+    }
+
+    return json_decode($output, true);
+}
+
+try {
+    // Get access token once
+    $access_token = getAccessToken($username, $password);
+
+    // Step 1: Check if timeout is marked and get attid
+    $timeoutCheckData = ["attid" => "0", "hrmsEmpId" => 119245, "ulbId" => 707];
+    $timeoutCheckUrl = "http://210.89.42.212:9999/HRMSAPI/hrms/rest/geo/emp/attendance/ismark/timeoutV1/det";
+
+    $timeoutResponse = postCurl($timeoutCheckUrl, $timeoutCheckData, $access_token);
+
+    if (!isset($timeoutResponse['attid'])) {
+        throw new Exception("Attendance ID not found in timeout check response");
+    }
+
+    $attId = $timeoutResponse['attid'];
+
+    // Step 2: Mark Out attendance
+    $markOutData = [
+        "attid" => $attId,
+        "outtimeLat" => "18.5199552",
+        "outtimeLon" => "73.778571",
+        "hrmsEmpId" => 119245,
+        "outtime_locationName" => "NDA Pashan Road, Bavdhan, Pune, Maharashtra, India",
+        "ulbId" => 707,
+        "deviceFrom" => "I",
+        "deviceImeiNoIn" => "00008110-000135E40A07A01E",
+        "locationName" => "null"
+    ];
+
+    $markOutUrl = "http://210.89.42.212:9999/HRMSAPI/hrms/rest/geo/save/emp/attendance/outtime";
+
+    $markOutResponse = postCurl($markOutUrl, $markOutData, $access_token);
+
+    echo json_encode($markOutResponse);
+
+} catch (Exception $e) {
+    echo json_encode([
+        "error" => true,
+        "message" => $e->getMessage()
+    ]);
+}
+
 ?>
